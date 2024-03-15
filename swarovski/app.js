80e8c98ac3b45c0256853ef7ef3b7a3f6b857d3c
Alwin <alwinpimathew@gmail.com>
2024-03-15 11:33:08 +0530

    first commit

---
+++ swarovski/app.js
@@ -0,0 +1,23 @@
+window.onscroll = function () {
+  myFunction();
+};
+
+var navbar = document.getElementById("section_bottom");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.scrollY >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+// nav code
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
