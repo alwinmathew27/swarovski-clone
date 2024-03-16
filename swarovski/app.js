f09c171bf557b9bbb658c6d5f8436c03b98f782a
Alwin <alwinpimathew@gmail.com>
2024-03-16 18:24:06 +0530

    slider update

--- swarovski/app.js
+++ swarovski/app.js
@@ -21,3 +21,44 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
+
+// slider-------------
+
+const productContainers = [...document.querySelectorAll(".product-container")];
+const nxtBtn = [...document.querySelectorAll(".nxt-btn")];
+const preBtn = [...document.querySelectorAll(".pre-btn")];
+
+productContainers.forEach((item, i) => {
+  let containerDimensions = item.getBoundingClientRect();
+  let containerWidth = containerDimensions.width;
+
+  nxtBtn[i].addEventListener("click", () => {
+    item.scrollLeft += containerWidth;
+  });
+
+  preBtn[i].addEventListener("click", () => {
+    item.scrollLeft -= containerWidth;
+  });
+});
+
+const searchBar = document.getElementById("searchBar");
+
+const productCards = document.querySelectorAll(".product-card");
+
+function searchProducts() {
+  const searchQuery = searchBar.value.toLowerCase();
+
+  productCards.forEach((card) => {
+    const productName = card
+      .querySelector(".product-brand")
+      .textContent.toLowerCase();
+
+    if (productName.includes(searchQuery)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+}
+
+searchBar.addEventListener("input", searchProducts);
