c95e20c226d8eda4c5731324e3552b376eb9a0b0
Alwin <alwinpimathew@gmail.com>
2024-03-17 19:17:34 +0530

    slider update

--- swarovski/app.js
+++ swarovski/app.js
@@ -1,3 +1,4 @@
+// header sticky-------
 window.onscroll = function () {
   myFunction();
 };
@@ -13,7 +14,7 @@ function myFunction() {
   }
 }
 
-// nav code
+// nav code----------------------------
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -21,8 +22,6 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
-
-// slider-------------
 
 const productContainers = [...document.querySelectorAll(".product-container")];
 const nxtBtn = [...document.querySelectorAll(".nxt-btn")];
@@ -40,13 +39,15 @@ productContainers.forEach((item, i) => {
     item.scrollLeft -= containerWidth;
   });
 });
-
+// search ----------------------
 const searchBar = document.getElementById("searchBar");
 
 const productCards = document.querySelectorAll(".product-card");
+searchBar.addEventListener("input", searchProducts);
 
 function searchProducts() {
   const searchQuery = searchBar.value.toLowerCase();
+  let visibleCardCount = 0;
 
   productCards.forEach((card) => {
     const productName = card
@@ -55,10 +56,26 @@ function searchProducts() {
 
     if (productName.includes(searchQuery)) {
       card.style.display = "block";
+      visibleCardCount++;
     } else {
       card.style.display = "none";
     }
   });
-}
 
-searchBar.addEventListener("input", searchProducts);
+  // Show empty text. if no visible cards
+  const emptyText = document.getElementById("empty-text");
+  if (visibleCardCount === 0) {
+    emptyText.style.display = "block";
+  } else {
+    emptyText.style.display = "none";
+  }
+
+  // button vilibility
+  if (visibleCardCount <= 4) {
+    preBtn.forEach((btn) => (btn.style.display = "none"));
+    nxtBtn.forEach((btn) => (btn.style.display = "none"));
+  } else {
+    preBtn.forEach((btn) => (btn.style.display = "block"));
+    nxtBtn.forEach((btn) => (btn.style.display = "block"));
+  }
+}
